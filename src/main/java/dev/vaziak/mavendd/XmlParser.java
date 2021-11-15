50f06eaf521d7765954de5f09203dbfd60241e39
Numzskull <23282729+Numzskull@users.noreply.github.com>
2021-11-15 08:43:16 -0500

    Added support for properties.

--- src/main/java/dev/vaziak/mavendd/XmlParser.java
+++ src/main/java/dev/vaziak/mavendd/XmlParser.java
@@ -2,6 +2,7 @@ package dev.vaziak.mavendd;
 
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
+import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 import org.xml.sax.SAXException;
 
@@ -10,6 +11,8 @@ import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
 import java.io.File;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 public class XmlParser {
     public ParsedPom parseXml(File pomFile) throws IOException, SAXException, ParserConfigurationException {
@@ -18,9 +21,26 @@ public class XmlParser {
         Document document = documentBuilder.parse(pomFile);
 
         ParsedPom parsedPom = new ParsedPom();
+        Map<String, String> propertyValues = new HashMap<>();
 
+        NodeList propertiesNodes = document.getDocumentElement().getElementsByTagName("properties");
         NodeList repositoryNodes = document.getDocumentElement().getElementsByTagName("repositories");
         NodeList dependencyNodes = document.getDocumentElement().getElementsByTagName("dependencies");
+
+        Element propertyElement;
+
+        if (propertiesNodes != null) {
+            propertyElement = (Element) propertiesNodes.item(0);
+
+            for (int i = 0; i < propertyElement.getChildNodes().getLength(); i++) {
+                Node node = propertyElement.getChildNodes().item(i);
+
+                if (node != null && !node.getNodeName().equals("#text")
+                        && node.getTextContent().length() > 0) {
+                    propertyValues.put(node.getNodeName(), node.getTextContent().trim());
+                }
+            }
+        }
 
         if (repositoryNodes != null) {
             Element repositoryElement = (Element) repositoryNodes.item(0);
@@ -42,10 +62,30 @@ public class XmlParser {
                 NodeList versionList = dependencyElement.getElementsByTagName("version");
 
                 for (int i = 0; i < groupIdList.getLength(); i++) {
+                    String groupId = artifactIdList.item(i).getTextContent();
+                    String artifactId = artifactIdList.item(i).getTextContent();
+                    String version = versionList.item(i).getTextContent();
+
+
+                    if (groupId.startsWith("${") && groupId.endsWith("}")) {
+                        groupId = groupId.replace("${", "").replace("}", "");
+                        groupId = propertyValues.get(groupId);
+                    }
+
+                    if (artifactId.startsWith("${") && artifactId.endsWith("}")) {
+                        artifactId = artifactId.replace("${", "").replace("}", "");
+                        artifactId = propertyValues.get(artifactId);
+                    }
+
+                    if (version.startsWith("${") && version.endsWith("}")) {
+                        version = version.replace("${", "").replace("}", "");
+                        version = propertyValues.get(version);
+                    }
+
                     parsedPom.addDependency(new ParsedPom.Dependency(
-                            groupIdList.item(i).getTextContent(),
-                            artifactIdList.item(i).getTextContent(),
-                            versionList.item(i).getTextContent()
+                            groupId,
+                            artifactId,
+                            version
                     ));
                 }
             }
